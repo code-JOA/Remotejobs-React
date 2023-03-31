4d2d5705fdaa10aa021fb5ff064e1dd720d5eacd
Joshua Owusu <joshbillion1@gmail.com>
2023-03-31 14:50:53 +0000

    Commit auto_update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'blueColor' : "#2a68ff",
         'greyIsh' : '#f1f4f8',
         'cardShadow' : '#f7f8f9',
-        'textColor' : #
+        'textColor' : '#252b36',
       }
     },
   },
