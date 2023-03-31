bb4e1ce82c1b847c72d6f388174bba5c4d6291b2
Joshua Owusu <joshbillion1@gmail.com>
2023-03-31 14:46:43 +0000

    Commit auto_update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bluecolor' : "blue",
+      }
+    },
   },
   plugins: [],
 }
