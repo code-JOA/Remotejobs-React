41c851c095cd3c401fec3ec18828ea3ea31a4a5e
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 09:10:31 +0000

    Commit auto_update

--- remotejobs/tailwind.config.js
+++ remotejobs/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      color: 
+    },
   },
   plugins: [],
 }
