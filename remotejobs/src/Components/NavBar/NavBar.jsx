f3e97563c70d71b44eec191778c368e2a6708e57
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 09:46:58 +0000

    Commit auto_update

--- remotejobs/src/Components/NavBar/NavBar.jsx
+++ remotejobs/src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='navBar flex justify-between items-center p-[3rem]'>
-    <div className>
+    <div className='logoDiv'>
+      <
       </div> 
 
     </div>
