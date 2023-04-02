d9a4abdff5cdb860257f001b6df30ebbdf12526a
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 13:31:12 +0000

    Commit auto_update

--- remotejobs/tailwind.config.js
+++ remotejobs/tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 // export default {
-  export const content = [
-  "./index.html",
-  "./src/**/*.{js,jsx}",
-];
-export const theme = {
-  extend: {
-    colors: {
-      'blueColor': '#2a68ff',
-      'greyIsh': '#f1f4f8',
-      'cardShadow': '#f7f8f9',
-      'textColor': '#252b36',
-    }
+  module.exports =
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'blueColor': '#2a68ff',
+        'greyIsh': '#f1f4f8',
+        'cardShadow': '#f7f8f9',
+        'textColor': '#252b36',
+      } 
+    },
   },
-};
-export const plugins = [];
+  plugins: [],
+}
 
