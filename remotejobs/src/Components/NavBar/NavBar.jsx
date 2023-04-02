2101843d158b78f3d220295efc804dcddc450d7c
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 11:47:09 +0000

    Commit auto_update

--- remotejobs/src/Components/NavBar/NavBar.jsx
+++ remotejobs/src/Components/NavBar/NavBar.jsx
@@ -4,8 +4,7 @@ const NavBar = () => {
   return (
     <div className='navBar flex justify-between items-center p-[3rem]'>
 <div className='logoDiv'>
-  <h1 className='logo'>
-
+  <h1 className='logo'>Remote<strong>Jobs</strong>
   </h1>
 </div>
     </div>
