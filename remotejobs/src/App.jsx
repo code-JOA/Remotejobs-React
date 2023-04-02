36aa74080a4a8d9de388599990e170ea1f1caa45
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 06:51:04 +0000

    Commit auto_update

--- remotejobs/src/App.jsx
+++ remotejobs/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+
 
 const App = () => {
   return (
     <div>"App"</div>
-    <p>Tailwind css works too</p>
+    // <p>Tailwind css works too</p>
   )
 }
 
