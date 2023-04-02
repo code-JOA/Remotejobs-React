04ea771e6fdfc1c8d1c2ad000a118517df523938
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 07:33:53 +0000

    Commit auto_update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // content: [
-  //    "./index.html",
-  //   "./src/**/*.{js,jsx}",
-  // ],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+     "./index.html",
+    "./src/**/*.{js,jsx}",
   ],
+  // content: [
+  //   "./index.html",
+  //   "./src/**/*.{js,ts,jsx,tsx}",
+  // ],
   theme: {
     extend: {
       colors: {
