c6ff8c16939b66eea4d402d1759084ac82cf1174
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 11:05:53 +0000

    Commit auto_update

--- remotejobs/src/Components/NavBar/NavBar.jsx
+++ remotejobs/src/Components/NavBar/NavBar.jsx
@@ -8,15 +8,15 @@ const NavBar = () => {
         <strong>Job</strong>Search</h1>
       </div> 
 
-      <div className="menu flex gap-100">
+      {/* <div className="menu flex gap-100">
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs
-        {/* <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">About
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact
         <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register */}
+        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register
         </li>
-      </div>
+      </div> */}
     </div>
   )
 }
