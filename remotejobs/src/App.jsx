aae92f2c597687a0d65f9e4b0fafd5b2b9bdb31d
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 09:39:36 +0000

    Commit auto_update

--- remotejobs/src/App.jsx
+++ remotejobs/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Jobs from './Components/JobDiv/Jobs'
 import NavBar from './Components/NavBar/NavBar'
 import Search from './Components/SearchDiv/Search'
+import Value from './Components'
 
 const App = () => {
   return (
