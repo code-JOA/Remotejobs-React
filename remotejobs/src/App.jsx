94de7373d8661dad75ebc797713540e3505600c0
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 09:34:21 +0000

    Commit auto_update

--- remotejobs/src/App.jsx
+++ remotejobs/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const App = () => {
   return (
     <div>
-      <NavBar
+      <NavBar/>
     </div>
   //   <h1 className="text-3xl font-bold underline">
   //   Hello world!
