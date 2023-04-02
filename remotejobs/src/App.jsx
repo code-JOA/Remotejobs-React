ea3b99f09caca692e4aeaa35b1b3485797cb1f1b
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 09:20:53 +0000

    Commit auto_update

--- remotejobs/src/App.jsx
+++ remotejobs/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    // <div>New app</div>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+    </div>
+  //   <h1 className="text-3xl font-bold underline">
+  //   Hello world!
+  // </h1>
 
   )
 }
