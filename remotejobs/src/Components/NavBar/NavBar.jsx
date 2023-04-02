0d56151e17fa73d33d647446e6bf6f279bb943b6
Joshua Owusu <joshbillion1@gmail.com>
2023-04-02 10:13:55 +0000

    Commit auto_update

--- remotejobs/src/Components/NavBar/NavBar.jsx
+++ remotejobs/src/Components/NavBar/NavBar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='navBar flex justify-between items-center p-[3rem]'>
-    <div className="logo">
-      <h1 className="logo text-[25px] text-[]">
+    <div className="logoDiv">
+      <h1 className="logo text-[25px] text-blueColor">
         <strong>Remote</strong>Jobs</h1>
       </div> 
 
