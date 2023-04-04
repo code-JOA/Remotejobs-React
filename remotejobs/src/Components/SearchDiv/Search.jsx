73340078df26095a05de299631a7da1e4bb989e4
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 06:08:53 +0000

    added react-icons

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 const Search = () => {
   return (
-    <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[1rem]'>
+    <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
       <form action=''>
         <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px]
         bg-white p-5 shadow-lg shadow-greyIsh-700'>
         </div>
 
 <div className='flex gap-2 items-center'>
-
+  <AiOutlineSearch className='text-[25px] icon'/>
 </div>
 
       </form>
