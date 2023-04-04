ae2b70397e061a20f9406e8a8860232d1186769a
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 09:26:11 +0000

    Commit auto_update

--- remotejobs/src/Components/JobDiv/jobs.jsx
+++ remotejobs/src/Components/JobDiv/jobs.jsx
@@ -7,7 +7,11 @@ const Jobs = () => {
       py-10'>
         <div className='group/item singleJob w-[250px] p-[20px] bg-white rounded
         [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
-          
+
+        <span className>
+
+        </span>
+
         </div>
       </div>
     </div>
