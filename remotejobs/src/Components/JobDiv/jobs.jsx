f006a12cc1ccaec51cc23b022a02312b69b280d4
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 09:21:58 +0000

    Commit auto_update

--- remotejobs/src/Components/JobDiv/jobs.jsx
+++ remotejobs/src/Components/JobDiv/jobs.jsx
@@ -5,7 +5,7 @@ const Jobs = () => {
     <div>
       <div className='jobcontainer flex gap-10 justify-center flex-wrap items-center
       py-10'>
-        <div className='group/item single'></div>
+        <div className='group/item singleJob w-[250px] p-20 bg-white '></div>
       </div>
     </div>
   )
