4c773e83ccb25725ad1f99cb58c33fe15d1aaf43
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 12:54:58 +0000

    Commit auto_update

--- remotejobs/src/Components/JobDiv/jobs.jsx
+++ remotejobs/src/Components/JobDiv/jobs.jsx
@@ -54,8 +54,8 @@ const Jobs = () => {
       {
         data.map(({id,image,title,time,location,desc,company}) => {
           return (
-            <div key={id} className='group group/item singleJob w-[250px] p-[20px] bg-white rounded
-            [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
+            <div key={id} className='group group/item singleJob w-[250px] p-[20px] bg-white 
+            rounded [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
     
             <span className='flex justify-between items-center gap-4 '>
               <h1 className='text-[16px] font-semibold text-textColor
