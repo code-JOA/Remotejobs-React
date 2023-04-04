55affafc00da6c3e6dc9a4930a8ec46b7ce6473f
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 06:25:54 +0000

    Commit auto_update

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -5,13 +5,20 @@ const Search = () => {
   return (
     <div className='searchDiv grid gp-10 bg-greyIsh rounded-[10px] p-[3rem]'>
       <form action=''>
-        <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px]
-        bg-white p-5 shadow-lg shadow-greyIsh-700'>
-        </div>
+
+        <div className='firstDiv flex justify-between items-center rounded-[8px] 
+        gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700'>
+
 
 <div className='flex gap-2 items-center'>
   <AiOutlineSearch className='text-[25px] icon'/>
+  <input type='text' className='bg-transparent text-blue-500 focus:outline-none 
+   w-100%' placeholder='SearchJobs Here....' />
+   <AiOut/>
 </div>
+
+</div>
+
 
       </form>
       </div>
