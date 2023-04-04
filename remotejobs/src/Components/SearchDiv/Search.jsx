e814a28f5da4539438b477bc700f558987d42c7b
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 07:01:15 +0000

    Commit auto_update

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -17,10 +17,9 @@ const Search = () => {
 
           <div className='flex gap-2 items-center'>
             <AiOutlineSearch className='text-[25px] icon'/>
-            <input type='text' className='bg-transparent text-blue-500 focus:outline-none 
-          w-100%' placeholder='SearchJobs Here....' />
-
-      <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6]
+            <input type='text' className='bg-transparent text-blue-500 
+            focus:outline-none w-[100%]' placeholder='SearchJobs Here....' />
+              <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6]
           hover:text-textColor icon' />
     </div>
 
