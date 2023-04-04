af603ab40446b84fe12c15ea56a393429c79d437
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 13:02:11 +0000

    Commit auto_update

--- remotejobs/src/Components/JobDiv/jobs.jsx
+++ remotejobs/src/Components/JobDiv/jobs.jsx
@@ -67,12 +67,12 @@ const Jobs = () => {
     
             <h6 className='text-[#ccc]'>{location}</h6>
             <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px]
-            group-hover:text-white'>{Lorem empire, Lorem ipsum dolor sit amet, consectet
+            group-hover:text-white'>{desc}
             </p>
     
             <div className="company flex items-center gap-2">
-            <img src={logo1} alt='Company Logo' className='w-[40%]'/>
-            <span className='text-[14px] py-[1rem] block group-hover:text-white'>Bravo
+            <img src={image} alt='Company Logo' className='w-[40%]'/>
+            <span className='text-[14px] py-[1rem] block group-hover:text-white'>{company}
             </span>
             </div>
     
