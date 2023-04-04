25ca1f0460012e71b2699575c99bceea9fd409fc
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 09:19:52 +0000

    Commit auto_update

--- remotejobs/src/Components/JobDiv/jobs.jsx
+++ remotejobs/src/Components/JobDiv/jobs.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Jobs = () => {
   return (
     <div>
-      <div className='jobcontainer flex gap-10 justify-center item'></div>
+      <div className='jobcontainer flex gap-10 justify-center flex-wrap items-center
+      py-10'></div>
     </div>
   )
 }
