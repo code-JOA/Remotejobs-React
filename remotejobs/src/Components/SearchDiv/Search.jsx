50faa8a16208e5ed7d8a6c804e08da4391b93c12
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 08:53:17 +0000

    Commit auto_update

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -64,12 +64,12 @@ const Search = () => {
             <label htmlFor="relevance" className='text-[#808080] font-semibold'>Sort 
             by:</label>
 
-           <select name="" id="relevance" className='bg-white rounded-[3px] px-4 py-1'>
-           <option value="">Relevance</option>
-           <option value="">Inclusive</option>
-   <option value="">StartsWith</option>
-   <option value="">Contains</option>
-    </select>
+            <select name="" id="relevance" className='bg-white rounded-[3px] px-4 py-1'>
+              <option value="">Relevance</option>
+              <option value="">Inclusive</option>
+              <option value="">StartsWith</option>
+              <option value="">Contains</option>
+            </select>
 
 </div>
 
