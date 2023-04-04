94e0596a2a81c3f45ff5e976d5d1729e27be3331
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 06:34:20 +0000

    Commit auto_update

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -16,7 +16,8 @@ const Search = () => {
   <input type='text' className='bg-transparent text-blue-500 focus:outline-none 
    w-100%' placeholder='SearchJobs Here....' />
 
-   <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6] hover:text-color '/>
+   <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6]
+    hover:text-textColor icons' />
 
 </div>
 
