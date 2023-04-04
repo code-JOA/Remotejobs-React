bbf4503bb30cfd2f4013596ca57628bc5774be3e
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 12:51:48 +0000

    Commit auto_update

--- remotejobs/src/Components/JobDiv/jobs.jsx
+++ remotejobs/src/Components/JobDiv/jobs.jsx
@@ -53,34 +53,36 @@ const Jobs = () => {
       
       {
         data.map(({id,image,title,time,location,desc,company}) => {
-          <div className='group group/item singleJob w-[250px] p-[20px] bg-white rounded
-          [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
-  
-          <span className='flex justify-between items-center gap-4 '>
-            <h1 className='text-[16px] font-semibold text-textColor
-              group-hover:text-white'>Web Developer</h1>
-            <span className='flex items-center text-[#ccc] gap-1 '>
-              <BiTimeFive/>Now
+          return (
+            <div className='group group/item singleJob w-[250px] p-[20px] bg-white rounded
+            [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
+    
+            <span className='flex justify-between items-center gap-4 '>
+              <h1 className='text-[16px] font-semibold text-textColor
+                group-hover:text-white'>Web Developer</h1>
+              <span className='flex items-center text-[#ccc] gap-1 '>
+                <BiTimeFive/>Now
+              </span>
             </span>
-          </span>
-  
-          <h6 className='text-[#ccc]'>Canada</h6>
-          <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px]
-          group-hover:text-white'>Lorem empire, Lorem ipsum dolor sit amet, consectet
-          </p>
-  
-          <div className="company flex items-center gap-2">
-          <img src={logo1} alt='Company Logo' className='w-[40%]'/>
-          <span className='text-[14px] py-[1rem] block group-hover:text-white'>Bravo
-          </span>
-          </div>
-  
-          <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-[14px]
-          semi-bold text-textColor hover:bg-white group-hover/item:text-textColor
-          group-hover:text-white'>
-          Apply Now
-          </button>
-          </div>
+    
+            <h6 className='text-[#ccc]'>Canada</h6>
+            <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px]
+            group-hover:text-white'>Lorem empire, Lorem ipsum dolor sit amet, consectet
+            </p>
+    
+            <div className="company flex items-center gap-2">
+            <img src={logo1} alt='Company Logo' className='w-[40%]'/>
+            <span className='text-[14px] py-[1rem] block group-hover:text-white'>Bravo
+            </span>
+            </div>
+    
+            <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-[14px]
+            semi-bold text-textColor hover:bg-white group-hover/item:text-textColor
+            group-hover:text-white'>
+            Apply Now
+            </button>
+            </div>
+          )
         })
       }
 
