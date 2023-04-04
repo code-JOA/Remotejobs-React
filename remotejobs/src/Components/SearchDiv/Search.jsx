d7069ba1f2ec2ba00f6d6172cd084423fb4b4852
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 06:58:01 +0000

    Commit auto_update

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -43,11 +43,8 @@ const Search = () => {
     </div>
 
     <button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white 
-cursor-pointer hover:bg-blue-300' >Search</button>
-
-
-
-</div>
+        cursor-pointer hover:bg-blue-300' >Search</button>
+    </div>
 
 
       </form>
