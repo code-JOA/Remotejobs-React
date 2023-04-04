e80cf7d8fa4ae52a899b76af1ba128313a1eeec0
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 06:56:43 +0000

    Commit auto_update

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -24,25 +24,25 @@ const Search = () => {
           hover:text-textColor icon' />
     </div>
 
-<div className='flex gap-2 items-center'>
-  <BsHouseDoor className='text-[25px] icon'/>
-  <input type='text' className='bg-transparent text-blue-500 focus:outline-none 
-   w-[100%]' placeholder='Search by Company....' />
+    <div className='flex gap-2 items-center'>
+      <BsHouseDoor className='text-[25px] icon'/>
+        <input type='text' className='bg-transparent text-blue-500 focus:outline-none 
+          w-[100%]' placeholder='Search by Company....' />
 
-   <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6]
-    hover:text-textColor icon' />
-</div>
+      <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6]
+        hover:text-textColor icon' />
+    </div>
 
-<div className='flex gap-2 items-center'>
-  <CiLocationOn className='text-[25px] icon'/>
-  <input type='text' className='bg-transparent text-blue-500 focus:outline-none 
-   w-100%' placeholder='Search by Location....' />
+    <div className='flex gap-2 items-center'>
+      <CiLocationOn className='text-[25px] icon'/>
+        <input type='text' className='bg-transparent text-blue-500 focus:outline-none 
+          w-100%' placeholder='Search by Location....' />
 
-   <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6]
-    hover:text-textColor icon' />
-</div>
+      <AiOutlineCloseCircle className='tex-[30px] text-[#a5a6a6]
+        hover:text-textColor icon' />
+    </div>
 
-<button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white 
+    <button className='bg-blueColor h-full p-5 px-10 rounded-[10px] text-white 
 cursor-pointer hover:bg-blue-300' >Search</button>
 
 
