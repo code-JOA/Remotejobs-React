ad6629a5c579403944ccd7836dbffb06ed9e4362
Joshua Owusu <joshbillion1@gmail.com>
2023-04-04 08:52:13 +0000

    Commit auto_update

--- remotejobs/src/Components/SearchDiv/Search.jsx
+++ remotejobs/src/Components/SearchDiv/Search.jsx
@@ -58,8 +58,21 @@ const Search = () => {
 
             </div>
 
+            <div className='secDiv flex items-center gap-10 justify-center'>
 
-            
+            <div className='singleSearch flex items-center gap-2'>
+            <label htmlFor="relevance" className='text-[#808080] font-semibold'>Sort 
+            by:</label>
+
+           <select name="" id="relevance" className='bg-white rounded-[3px] px-4 py-1'>
+           <option value="">Relevance</option>
+           <option value="">Inclusive</option>
+   <option value="">StartsWith</option>
+   <option value="">Contains</option>
+    </select>
+
+</div>
+
           </div>
 
       </div>
