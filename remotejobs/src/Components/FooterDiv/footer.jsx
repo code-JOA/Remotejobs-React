f6cea000dd96fd4c197eff22034cc003892a29fb
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 15:21:12 +0000

    Commit auto_update

--- remotejobs/src/Components/FooterDiv/footer.jsx
+++ remotejobs/src/Components/FooterDiv/footer.jsx
@@ -21,7 +21,11 @@ const Footer = () => {
           Company
         </span>
         <div className='grid gap-3'>
-          <li className='text-white opacity-[.7] hover: '>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1] '>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1] '>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1] '>About Us</li>
+          <li className='text-white opacity-[.7] hover:opacity-[1] '>About Us</li>
+
         </div>
       </div>
 
