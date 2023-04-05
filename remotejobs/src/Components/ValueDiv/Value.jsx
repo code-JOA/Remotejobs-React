a31f558b2e5ca7b44e608bcc1b31b6c3d1e043d9
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 04:10:15 +0000

    Commit auto_update

--- remotejobs/src/Components/ValueDiv/Value.jsx
+++ remotejobs/src/Components/ValueDiv/Value.jsx
@@ -27,25 +27,7 @@ const Value = () => {
           </p>
         </div>
       </div>
-
-      <div className='grid gap-[10rem] grid-cols-3 items-center'>
-        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
-          <div className='flex items-center gap-3'>
-            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] 
-              h-[40px] w-[40px] flex items-center justify-center'>
-              <img src={unedited} alt="" className='w-[70%]'/>
-            </div>
-
-            <span className='font-semibold text-textColor text-[18px]'>
-              Something
-            </span>
-          </div>
-          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold '>
-            We believe in making things better for everyone to live comfortably
-          </p>
-        </div>
-      </div>
-
+      
     </div>
   )
 }
