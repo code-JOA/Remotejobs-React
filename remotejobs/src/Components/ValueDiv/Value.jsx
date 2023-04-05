d5d44afa7402ad5aa56f389443ef37ffca75d8a7
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 04:26:54 +0000

    Commit auto_update

--- remotejobs/src/Components/ValueDiv/Value.jsx
+++ remotejobs/src/Components/ValueDiv/Value.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo9 from '../../Assets/logo9.png'
 import unedited from '../../Assets/unedited.png'
+import new from '../../Assets/'
 
 
 const Value = () => {
@@ -50,7 +51,7 @@ const Value = () => {
           <div className='flex items-center gap-3'>
             <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] 
               h-[40px] w-[40px] flex items-center justify-center'>
-              <img src={logo9} alt="" className='w-[70%]'/>
+              <img src={new} alt="" className='w-[70%]'/>
             </div>
 
             <span className='font-semibold text-textColor text-[18px]'>
