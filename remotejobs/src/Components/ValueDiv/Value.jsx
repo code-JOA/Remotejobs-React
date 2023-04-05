532e6aa95119e834b31f9e555b55b400eecd0cb7
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 04:13:23 +0000

    Commit auto_update

--- remotejobs/src/Components/ValueDiv/Value.jsx
+++ remotejobs/src/Components/ValueDiv/Value.jsx
@@ -27,7 +27,24 @@ const Value = () => {
             Minimalist and beauty are our hallmarks in everything we do on a daily basis
           </p>
         </div>
-        
+
+        {/* second grid goes here */}
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] 
+              h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={logo9} alt="" className='w-[70%]'/>
+            </div>
+
+            <span className='font-semibold text-textColor text-[18px]'>
+              Simplicity
+            </span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold '>
+            Minimalist and beauty are our hallmarks in everything we do on a daily basis
+          </p>
+        </div>
       </div>
     </div>
   )
