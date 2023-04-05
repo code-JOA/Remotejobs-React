deaa9483e4e1ca520687e900ce71746f66b845da
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 04:55:23 +0000

    Commit auto_update

--- remotejobs/src/Components/FooterDiv/footer.jsx
+++ remotejobs/src/Components/FooterDiv/footer.jsx
@@ -6,8 +6,9 @@ const Footer = () => {
     grid-cols-5 m-auto items-center justify-center'>
       <div>
         <div className='logoDiv'>
-          <h1 className="logo text-[25px] text-blueColor">
-          <strong>Remote</strong>Jobs</h1>
+          <h1 className="logo text-[25px] text-white pb-[1.5rem] ">
+          <strong>Remote</strong>Jobs
+          </h1>
 
         </div>
       </div>
