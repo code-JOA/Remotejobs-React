5d4fa5a8b56f138ecc657de97372a7e2c9c4c519
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 04:48:23 +0000

    Commit auto_update

--- remotejobs/src/Components/FooterDiv/footer.jsx
+++ remotejobs/src/Components/FooterDiv/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footer p-[5rem] mb-4 bg-[blueColor]'>
+    <div className='footer p-[5rem] mb-4 bg-blueColor rounded-10px gap-8 grid-cols-1'>
 
     </div>
   )
