db489c13a3a29434982ee980abe14606be314740
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 03:49:44 +0000

    Commit auto_update

--- remotejobs/src/Components/ValueDiv/Value.jsx
+++ remotejobs/src/Components/ValueDiv/Value.jsx
@@ -20,7 +20,7 @@ const Value = () => {
             </span>
           </div>
           <p className='text-[25px] text-textColor opacity-[.7p] py-[1rem] font-semibold '>
-            Minimalist and beauty are our hallmarks in everything we do
+            Minimalist and beauty are our hallmarks in everything we do on a daily basis
           </p>
         </div>
       </div>
