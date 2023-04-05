cbb7ad96e8234baf882cb6cf5e58074d8084c383
Joshua Owusu <joshbillion1@gmail.com>
2023-04-05 04:30:03 +0000

    Commit auto_update

--- remotejobs/src/Components/ValueDiv/Value.jsx
+++ remotejobs/src/Components/ValueDiv/Value.jsx
@@ -51,7 +51,7 @@ const Value = () => {
           <div className='flex items-center gap-3'>
             <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] 
               h-[40px] w-[40px] flex items-center justify-center'>
-              <img src={new} alt="" className='w-[70%]'/>
+              <img src={logo10} alt="" className='w-[70%]'/>
             </div>
 
             <span className='font-semibold text-textColor text-[18px]'>
