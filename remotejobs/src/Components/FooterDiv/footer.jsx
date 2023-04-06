33e153fb8ffd17ffa7739470e4bf494e817a2952
Joshua Owusu <joshbillion1@gmail.com>
2023-04-06 09:38:51 +0000

    Commit auto_update

--- remotejobs/src/Components/FooterDiv/footer.jsx
+++ remotejobs/src/Components/FooterDiv/footer.jsx
@@ -6,7 +6,7 @@ import { BsTwitter } from 'react-icons/bs'
 const Footer = () => {
   return (
     <div className='footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 
-    grid grid-cols-5 m-auto items-center justify-center'>
+    grid grid-cols-5 m-auto items-start justify-center'>
       <div>
         <div className='logoDiv'>
           <h1 className="logo text-[25px] text-white pb-[1.5rem]">
