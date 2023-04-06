e6c8566d76c82c21a574a8c86cb4a808228c5f6e
Joshua Owusu <joshbillion1@gmail.com>
2023-04-06 09:17:42 +0000

    Commit auto_update

--- remotejobs/src/Components/ValueDiv/Value.jsx
+++ remotejobs/src/Components/ValueDiv/Value.jsx
@@ -72,7 +72,10 @@ const Value = () => {
           Ready to Switch Careers?
           </h1>
           <h2 className='text-textColor text-[25px] font-bold '>Let's Get Started</h2>
-          
+          <button className='border-[2px] rounded-[10px] py-[4px] px-[50px] text-[1px]
+          text-semibold text-blueColor hover:bg-white border-blueColor'>
+          Get Started
+          </button>
 
         </div>
         
